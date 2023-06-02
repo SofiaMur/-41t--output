71d7ea01d1785eb5591604cc5094da9058dbc3c2
SofiaMur <115185146+SofiaMur@users.noreply.github.com>
2023-06-02 17:20:20 +0300

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Output from "./lib/Output.svelte";
+</script>
+
+<main>
+  <div class="flex-col">
+    <h1 class="font-bold p-3 text-xl">Новости</h1>
+    <Output />
+  </div>
+</main>
+
+<style>
+  * {
+    @apply flex items-center justify-center;
+  }
+</style>
---
+++ src/lib/Output.svelte
@@ -0,0 +1,60 @@
+<script>
+    import { onMount } from "svelte";
+    const endpoint = "https://jsonplaceholder.typicode.com/posts";
+    let articles = [];
+
+    onMount(async function () {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        articles = data;
+    });
+</script>
+
+<main>
+    <div class="container">
+        {#each articles as article}
+            <div class="card">
+                <!-- <img src={article.urlToImage} /> -->
+                <img
+                    src="https://media.minecraftstation.com/2020/05/2020-05-15_22.50.06-copy-768x677.png"
+                    alt="example"/>
+                <p class="card-text-title">
+                    {article.title}
+                </p>
+                <p class="card-text-body">
+                    {article.body}...
+                </p>
+                <!-- <a href={article.url} class="card-link">
+                    Читать далее...</a> -->
+                <a href="https://youtu.be/ClldamscA0I" class="card-link">
+                    Читать далее...</a>
+            </div>
+        {/each}
+    </div>
+</main>
+
+<style>
+    .container {
+        @apply inline-grid grid-cols-3 gap-2 p-3 border border-black;
+    }
+
+    .container > .card img {
+        @apply rounded-md;
+    }
+
+    .card {
+        @apply m-3 p-3 w-80 select-none shadow-md border rounded-tl-md rounded-r-md border-black;
+    }
+
+    .card-text-title {
+        @apply font-semibold text-center text-base my-2;
+    }
+
+    .card-text-body {
+        @apply text-sm line-clamp-4;
+    }
+
+    .card-link {
+        @apply text-xs italic;
+    }
+</style>
